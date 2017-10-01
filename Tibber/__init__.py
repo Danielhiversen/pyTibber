ff9d080c381ef9f72069f158308165c531159ea9
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-10-01 17:43:05 +0200

    price unit

--- Tibber/__init__.py
+++ Tibber/__init__.py
@@ -305,10 +305,9 @@ class TibberHome(object):
     def price_unit(self):
         """Return the price unit."""
         country = self.country
-        if contry = 'NO':
+        if contry == 'NO':
             return 'NOK/kWh'
-        elif contry = 'SE':
+        elif contry == 'SE':
             return 'SEK/kWh'
         else:
             return ''
-
