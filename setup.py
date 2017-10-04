02406ff71d9e10ecee3c84314b54bb72b596b366
Daniel Høyer Iversen <mail@dahoiv.net>
2017-10-04 08:28:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     packages = ['Tibber'],
     install_requires=['gql==0.1.0', 'aiohttp==2.2.5', 'async_timeout==1.4.0'],
     version = '0.1.1',
-    description = 'a library to communicate with Tibber',
+    description = 'A python3 library to communicate with Tibber',
     author='Daniel Hoyer Iversen',
     author_email='mail@dahoiv.net',
-    url='https://github.com/Danielhiversen/pyRFXtrx',
+    url='https://github.com/Danielhiversen/pyTibber',
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
