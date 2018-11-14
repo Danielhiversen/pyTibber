4a40bfb6cb2263815071e1d6642dd389bde7d0b1
Daniel Høyer Iversen <mail@dahoiv.net>
2018-11-14 20:00:20 +0100

    Improve subscription_manager

--- tibber/subscription_manager.py
+++ tibber/subscription_manager.py
@@ -39,12 +39,13 @@ class SubscriptionManager:
         self._state = STATE_STARTING
         self._cancel_client_task()
         self._client_task = self.loop.create_task(self.running())
-        for subscription_id in range(len(self.subscriptions)):
-            callback, sub_query = self.subscriptions.get(subscription_id, (None, None))
+        for subscription_id in self.subscriptions:
+            callback, sub_query = self.subscriptions.pop(subscription_id, (None, None))
+            _LOGGER.debug("Removed, %s", subscription_id)
             if callback is None:
                 continue
-            _LOGGER.debug('Add subscription id %s.', subscription_id)
-            self.loop.create_task(self.subscribe(sub_query, callback, subscription_id))
+            _LOGGER.debug('Add subscription %s', callback)
+            self.loop.create_task(self.subscribe(sub_query, callback))
 
     @property
     def is_running(self):
@@ -55,24 +56,25 @@ class SubscriptionManager:
         """Start websocket connection."""
         await self._close_websocket()
         try:
-            _LOGGER.debug("Running")
+            _LOGGER.debug("Starting")
             self.websocket = await websockets.connect(self._url,
                                                       subprotocols=["graphql-subscriptions"])
             self._state = STATE_RUNNING
+            _LOGGER.debug("Running")
             self.websocket.send(json.dumps({"type": "init",
                                             "payload": self._init_payload}))
 
-            while self._state != STATE_STOPPED:
+            while self._state == STATE_RUNNING:
                 try:
-                    msg = await asyncio.wait_for(self.websocket.recv(), timeout=20)
+                    msg = await asyncio.wait_for(self.websocket.recv(), timeout=4)
                 except asyncio.TimeoutError:
-                    _LOGGER.error("No websocket data in 20 seconds, checking the connection.")
+                    _LOGGER.warning("No websocket data in 20 seconds, checking the connection.")
                     try:
                         pong_waiter = await self.websocket.ping()
-                        await asyncio.wait_for(pong_waiter, timeout=10)
+                        await asyncio.wait_for(pong_waiter, timeout=2)
                     except asyncio.TimeoutError:
-                        _LOGGER.debug("No response to ping in 10 seconds, reconnecting.")
-                        break
+                        _LOGGER.error("No response to ping in 10 seconds, reconnecting.")
+                        return
                     continue
                 await self._process_msg(msg)
         except Exception:  # pylint: disable=broad-except
@@ -85,6 +87,7 @@ class SubscriptionManager:
                 self.retry()
             else:
                 self._state = STATE_STOPPED
+            _LOGGER.debug("Closing running task.")
 
     async def stop(self, timeout=10):
         """Close websocket connection."""
@@ -120,25 +123,25 @@ class SubscriptionManager:
             _LOGGER.debug("Skip retry since state: %s", self._state)
             return
         self._cancel_retry_timer()
-        self._cancel_client_task()
         self._state = STATE_STARTING
         self._retry_timer = self.loop.call_later(self._wait_time_before_retry, self.start)
         _LOGGER.debug('Reconnecting to server in %i seconds.', self._wait_time_before_retry)
 
-    async def subscribe(self, sub_query, callback, current_session_id=None):
+    async def subscribe(self, sub_query, callback):
         """Add a new subscription."""
-        if current_session_id is None:
-            current_session_id = self._session_id
-            self._session_id += 1
-        if self.websocket is None or not self.websocket.open:
-            await asyncio.sleep(1, loop=self.loop)
-            return await self.subscribe(sub_query, callback, current_session_id)
 
+        if self.websocket is None or not self.websocket.open or not self._state == STATE_RUNNING:
+            await asyncio.sleep(1, loop=self.loop)
+            return await self.subscribe(sub_query, callback)
+
+        current_session_id = self._session_id
+        self._session_id += 1
         subscription = {"query": sub_query,
                         "type": "subscription_start", "id": current_session_id}
         json_subscription = json.dumps(subscription)
         await self.websocket.send(json_subscription)
         self.subscriptions[current_session_id] = (callback, sub_query)
+        _LOGGER.debug("New subscription %s", current_session_id)
         return current_session_id
 
     async def unsubscribe(self, subscription_id):
@@ -148,6 +151,7 @@ class SubscriptionManager:
             return
         await self.websocket.send(json.dumps({"id": subscription_id,
                                               "type": "subscription_end"}))
+        self.subscriptions.pop(subscription_id)
 
     async def _close_websocket(self):
         if self.websocket is None:
@@ -160,18 +164,20 @@ class SubscriptionManager:
     async def _process_msg(self, msg):
         """Process received msg."""
         result = json.loads(msg)
+        _LOGGER.debug("Recv, %s", result)
 
         subscription_id = result.get('id')
         if subscription_id is None:
+            _LOGGER.debug('Id %s.', subscription_id)
             return
 
         callback, _ = self.subscriptions.get(subscription_id, (None, None))
         if callback is None:
+            _LOGGER.debug('Unknown id %s.', subscription_id)
             return
 
         if result.get('type', '') == 'complete':
             _LOGGER.debug('Unsubscribe %s successfully.', subscription_id)
-            del self.subscriptions[subscription_id]
             return
 
         data = result.get('payload', {})
@@ -182,7 +188,10 @@ class SubscriptionManager:
 
     def _cancel_retry_timer(self):
         if self._retry_timer:
-            self._retry_timer.cancel()
+            try:
+                self._retry_timer.cancel()
+            finally:
+                self._retry_timer = None
 
     def _cancel_client_task(self):
         if self._client_task:
