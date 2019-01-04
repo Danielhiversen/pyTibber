2ad528c9cffd6344eb0a1dae072aad2843ba497e
Daniel Høyer Iversen <mail@dahoiv.net>
2019-01-04 11:56:05 +0100

    0.9.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=['gql>=0.1.0', 'aiohttp>=3.0.6',
                       'async_timeout>=1.4.0', 'websockets>=6.0',
                       'graphql-subscription-manager'],
-    version='0.8.7',
+    version='0.9.0',
     description='A python3 library to communicate with Tibber',
     long_description=long_description,
     python_requires='>=3.5.3',
