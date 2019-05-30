fb2ff04a5a5dfed025f7144da512333051042017
Daniel Høyer Iversen <mail@dahoiv.net>
2019-05-30 09:34:15 +0200

    0.11.4

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup
 
-long_description = None
-with open("README.md", 'r') as fp:
-    long_description = fp.read()
-
 setup(
     name = 'pyTibber',
     packages = ['tibber'],
     install_requires=['gql>=0.1.0', 'aiohttp>=3.0.6',
                       'async_timeout>=1.4.0', 'websockets>=6.0',
                       'graphql-subscription-manager>=0.3.0'],
-    version='0.11.3',
+    version='0.11.4',
     description='A python3 library to communicate with Tibber',
-    long_description=long_description,
     python_requires='>=3.5.3',
     author='Daniel Hoyer Iversen',
     author_email='mail@dahoiv.net',
