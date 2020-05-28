f83809e3a7d2534dd231dc141b202a1003790b0b
Daniel Høyer Iversen <mail@dahoiv.net>
2020-05-28 11:02:56 +0200

    remove websockets as dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     install_requires=[
         "aiohttp>=3.0.6",
         "async_timeout>=1.4.0",
-        "websockets>=6.0",
         "graphql-subscription-manager>=0.3.5",
         "pytz",
         "python-dateutil",
