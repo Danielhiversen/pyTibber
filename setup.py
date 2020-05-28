43792db2d510e0bcbb6f718e57ebb0d7256172da
Daniel Høyer Iversen <mail@dahoiv.net>
2020-05-28 11:23:46 +0200

    0.15.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "pytz",
         "python-dateutil",
     ],
-    version="0.15.0",
+    version="0.15.1",
     description="A python3 library to communicate with Tibber",
     python_requires=">=3.5.3",
     author="Daniel Hoyer Iversen",
