e15f06feff0835512e3b9ccedac80a023c86c78c
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-10-14 10:48:17 +0200

    Version   (#96)
    * __version__
    
    * Update const.py
    
    * Update setup.py
    
    * version from const

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
 from setuptools import setup
+
+
+consts = {}
+with open(os.path.join("tibber", "const.py"), "r") as fp:
+    exec(fp.read(), consts)
 
 setup(
     name="pyTibber",
@@ -10,7 +16,7 @@ setup(
         "pytz",
         "python-dateutil",
     ],
-    version="0.15.7",
+    version=consts["__version__"],
     description="A python3 library to communicate with Tibber",
     python_requires=">=3.5.3",
     author="Daniel Hjelseth Hoyer",
