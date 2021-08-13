5c31bbe2853f23657cf41100cc252d8e4132a3a1
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-08-13 13:34:40 +0200

    DEMO_TOKEN

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -12,7 +12,7 @@ from graphql_subscription_manager import SubscriptionManager
 from .const import RESOLUTION_HOURLY, __version__
 
 DEFAULT_TIMEOUT = 15
-DEMO_TOKEN = "d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a"
+DEMO_TOKEN = "476c477d8a039529478ebd690d35ddd80e3308ffc49b59c65b142321aee963a4"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 SUB_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
 
