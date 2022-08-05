f5af07783f7bb2f9808376abc4b1033cba2dc231
Daniel Hjelseth Høyer <github@dahoiv.net>
2022-08-05 08:50:52 +0200

    refactor
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -205,7 +205,7 @@ class Tibber:
             return self._home_ids
         return self._all_home_ids
 
-    def get_homes(self, only_active: bool = True) -> list["TibberHome"]:
+    def get_homes(self, only_active: bool = True) -> list[TibberHome]:
         """Return list of Tibber homes."""
         return [
             home
@@ -213,7 +213,7 @@ class Tibber:
             if (home := self.get_home(home_id))
         ]
 
-    def get_home(self, home_id: str) -> Optional["TibberHome"]:
+    def get_home(self, home_id: str) -> Optional[TibberHome]:
         """Return an instance of TibberHome for given home id."""
         if home_id not in self._all_home_ids:
             _LOGGER.error("Could not find any Tibber home with id: %s", home_id)
