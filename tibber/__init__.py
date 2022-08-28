2ac307c2d204453a0104ddd4853ed1204d3c4adf
Daniel Hjelseth Høyer <mail@dahoiv.net>
2022-08-28 19:15:16 +0200

    update demo token (#190)

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -14,7 +14,7 @@ from .gql_queries import INFO, PUSH_NOTIFICAION
 from .tibber_home import TibberHome
 
 DEFAULT_TIMEOUT = 10
-DEMO_TOKEN = "476c477d8a039529478ebd690d35ddd80e3308ffc49b59c65b142321aee963a4"
+DEMO_TOKEN = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 SUB_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
 
