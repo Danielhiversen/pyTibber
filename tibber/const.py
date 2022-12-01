1a658452c36c11c7f31ddaf63ce21517668bc368
Daniel Hjelseth Høyer <mail@dahoiv.net>
2022-12-01 17:36:56 +0100

    0.26.2

--- tibber/const.py
+++ tibber/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Final
 
-__version__ = "0.26.1"
+__version__ = "0.26.2"
 
 RESOLUTION_HOURLY: Final = "HOURLY"
 RESOLUTION_DAILY: Final = "DAILY"
