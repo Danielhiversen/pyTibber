3621536ba18995a0c896382e4ff3f02aeee7a905
Daniel Hjelseth Høyer <github@dahoiv.net>
2023-01-16 22:22:59 +0100

    Improve error handling and reconnect
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- tibber/const.py
+++ tibber/const.py
@@ -2,10 +2,9 @@
 
 from __future__ import annotations
 
-from http import HTTPStatus
 from typing import Final
 
-__version__ = "0.26.9"
+__version__ = "0.26.8"
 
 RESOLUTION_HOURLY: Final = "HOURLY"
 RESOLUTION_DAILY: Final = "DAILY"
@@ -15,8 +14,3 @@ RESOLUTION_ANNUAL: Final = "ANNUAL"
 
 DEMO_TOKEN: Final = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
 API_ENDPOINT: Final = "https://api.tibber.com/v1-beta/gql"
-
-API_ERR_CODE_UNKNOWN = "UNKNOWN"
-API_ERR_CODE_UNAUTH = "UNAUTHENTICATED"
-HTTP_CODES_RETRIABLE = [HTTPStatus.TOO_MANY_REQUESTS, HTTPStatus.PRECONDITION_REQUIRED]
-HTTP_CODES_FATAL = [HTTPStatus.BAD_REQUEST]
