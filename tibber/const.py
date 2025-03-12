6e9752be3575a309ee8f91de13177c4f0aed6b42
n0l1n <vincent.wehrwein@rwth-aachen.de>
2025-03-12 16:24:08 +0000

    add Ruff noqa to demo token

--- tibber/const.py
+++ tibber/const.py
@@ -7,7 +7,7 @@ __version__ = "0.30.8"
 
 API_ENDPOINT: Final = "https://api.tibber.com/v1-beta/gql"
 DEFAULT_TIMEOUT: Final = 10
-DEMO_TOKEN: Final = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
+DEMO_TOKEN: Final = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE" # noqa: S105
 
 RESOLUTION_HOURLY: Final = "HOURLY"
 RESOLUTION_DAILY: Final = "DAILY"
