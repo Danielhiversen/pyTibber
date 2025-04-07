4ede10b04a4cb941ef774440ef535c1d8349c5ef
Daniel Hjelseth Høyer <mail@dahoiv.net>
2025-04-07 21:03:23 +0200

    0.31.0

--- tibber/const.py
+++ tibber/const.py
@@ -3,7 +3,7 @@
 from http import HTTPStatus
 from typing import Final
 
-__version__ = "0.30.8"
+__version__ = "0.31.0"
 
 API_ENDPOINT: Final = "https://api.tibber.com/v1-beta/gql"
 DEFAULT_TIMEOUT: Final = 10
