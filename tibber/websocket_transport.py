ec4da4bf3cf85794d8c8b60330b28a8d62f6f70e
Willem-Jan van Rootselaar <liudgervr@gmail.com>
2025-11-25 09:48:46 +0100

    Refactor logging configuration in TibberWebsocketsTransport to suppress verbose messages

--- tibber/websocket_transport.py
+++ tibber/websocket_transport.py
@@ -11,15 +11,17 @@ from websockets.asyncio.connection import State
 
 _LOGGER = logging.getLogger(__name__)
 
-# Reduce verbose logging from gql websocket transport
-logging.getLogger("gql.transport.websockets").setLevel(logging.WARNING)
-
 
 class TibberWebsocketsTransport(WebsocketsTransport):
     """Tibber websockets transport."""
 
     def __init__(self, url: str, access_token: str, user_agent: str, ssl: SSLContext | bool = True) -> None:
-        """Initialize TibberWebsocketsTransport."""
+        """Initialize TibberWebsocketsTransport.
+        Configures the gql.transport.websockets logger to WARNING level to suppress
+        verbose INFO-level messages (<<< and >>> websocket traffic logs).
+        """
+        logging.getLogger("gql.transport.websockets").setLevel(logging.WARNING)
+        
         super().__init__(
             url=url,
             init_payload={"token": access_token},
